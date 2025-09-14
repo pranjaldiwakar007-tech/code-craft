764b6be2270b5a0883a85828c1486a34de4ead67
Pranjal Diwakar <pranjaldiwakar4@gmail.com>
2025-09-15 05:00:14 +0530

    Fix: Clerk auth with Convex (updated domain)

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://clever-horse-36.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
